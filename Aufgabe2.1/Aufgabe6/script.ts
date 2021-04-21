3f6a9b88d71ac3462f23d0bf2ad706ead77e922e
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-21 22:51:43 +0200

    Create script.ts

---
+++ Aufgabe2.1/Aufgabe6/script.ts
@@ -0,0 +1,11 @@
+for (let i: number = 1; i <= 100; i++){
+    if(i % 3){
+       console.log("Fizz");
+    }
+    if(i % 5 && i !% 3){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
