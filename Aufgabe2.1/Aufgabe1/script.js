741f9169959abf1dc95eeea5c73069d31cb4804d
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-21 09:57:34 +0200

    Create script.js

---
+++ Aufgabe2.1/Aufgabe1/script.js
@@ -0,0 +1,14 @@
+"use strict";
+function a1() {
+    let x = "Alles";
+    debugger;
+    console.log(x);
+    func1();
+    console.log("Logo!");
+}
+a1();
+function func1() {
+    console.log("Klar?");
+}
+//Ausgegeben in a wird Alles Klar? Logo!
+//# sourceMappingURL=script.js.map
