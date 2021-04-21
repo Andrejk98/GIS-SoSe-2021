e1ef63f276b90a19348096668e4f0f6283cb5212
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-21 22:58:42 +0200

    Update script.ts

--- Aufgabe2.1/Aufgabe1/script.ts
+++ Aufgabe2.1/Aufgabe1/script.ts
@@ -2,13 +2,23 @@ function a1(): void {
     let x: string = "Alles";
     console.log(x);
     func1();
-    console.log("Logo!");
+    console.log(x);
+    func2();
+    console.log(x);
+    func3();
 }
 
 a1();
 
 function func1(): void {
+    console.log("Gute");
+}
+
+function func2(): void {
     console.log("Klar?");
 }
 
+function func3(): void {
+    console.log("Logo!");
+}
 //Ausgegeben in a wird Alles Klar? Logo!
