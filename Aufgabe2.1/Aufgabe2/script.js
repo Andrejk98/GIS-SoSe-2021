7bbdb84004f5faa32dbb8b2414a3f03d259d43d1
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-22 11:53:33 +0200

    Create script.js

---
+++ Aufgabe2.1/Aufgabe2/script.js
@@ -0,0 +1,11 @@
+"use strict";
+function a2() {
+    let i = 9;
+    do {
+        console.log(i);
+        i = i - 1;
+    } while (i > 0);
+}
+a2();
+//Ich lag richtig in der annahme, dass es von 9 auf 1 runterzählt
+//# sourceMappingURL=script.js.map
