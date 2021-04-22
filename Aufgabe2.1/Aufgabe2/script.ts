bfff7d209f9d0a72a448c313fd22674b6f039812
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-22 11:53:17 +0200

    Create script.ts

---
+++ Aufgabe2.1/Aufgabe2/script.ts
@@ -0,0 +1,12 @@
+function a2(): void {
+    let i: number = 9;
+
+    do {
+        console.log(i);
+        i = i - 1;
+    } while ( i > 0);
+}
+
+a2();
+
+//Ich lag richtig in der annahme, dass es von 9 auf 1 runterzählt
