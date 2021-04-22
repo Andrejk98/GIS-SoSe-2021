23f4085ac064c84d8a051ef4b019ce601ff8e331
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-22 11:53:11 +0200

    Update script.js

--- Aufgabe2.1/Aufgabe1/script.js
+++ Aufgabe2.1/Aufgabe1/script.js
@@ -2,12 +2,21 @@
 function a1() {
     let x = "Alles";
     console.log(x);
-    func1();
-    console.log("Logo!");
+    ausgabe1();
+    console.log(x);
+    ausgabe2();
+    console.log(x);
+    ausgabe3();
 }
 a1();
-function func1() {
+function ausgabe1() {
+    console.log("Gute!");
+}
+function ausgabe2() {
     console.log("Klar?");
+}
+function ausgabe3() {
+    console.log("Logo!");
 }
 //Ausgegeben in a wird Alles Klar? Logo!
 //# sourceMappingURL=script.js.map
