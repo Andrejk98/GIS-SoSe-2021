ba39b56451c44661a3e0bf46e8479492a226b910
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-25 12:42:49 +0200

    Update script.js

--- Aufgabe2.1/Aufgabe5/script.js
+++ Aufgabe2.1/Aufgabe5/script.js
@@ -1,2 +1,53 @@
 "use strict";
+//A
+function multiply() {
+    let x = 5;
+    let y = 6;
+    let z;
+    z = x * y;
+    console.log(z);
+}
+//B
+/*
+function max(): void {
+    let y: number = 5;
+    let u: number = 3;
+    if (y < u) {
+        console.log(u);
+    }
+    else {
+        console.log(y);
+    }
+}
+*/
+//C
+/*
+function count(): void {
+    let x: number = 0;
+    while ( x < 100 ) {
+        x++;
+        console.log(x);
+    }
+}
+count();
+*/
+//d
+/*
+function getRandomInt(_a: 0, _b: 10): number {
+    for (let i: number = 0; i < 100; i++) {
+        return Math.random();
+    }
+}
+*/
+//e
+/*
+function factorial(): number {
+    let n: number = 1;
+    while (n > 1) {
+        n = n * (n + 1);
+        console.log(n);
+    }
+    return n;
+}
+*/ 
 //# sourceMappingURL=script.js.map
