a79a9d06a1114296fbb1e668514362cafa05a026
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-28 17:40:50 +0200

    Update script.js

--- Aufgabe2.1/Aufgabe5/Aufgabe5c/script.js
+++ Aufgabe2.1/Aufgabe5/Aufgabe5c/script.js
@@ -1,9 +1,11 @@
 "use strict";
 function count() {
     let x = 0;
+    let a = 0;
     while (x < 100) {
         x++;
-        console.log(x);
+        a = x + a;
+        console.log(a);
     }
 }
 count();
