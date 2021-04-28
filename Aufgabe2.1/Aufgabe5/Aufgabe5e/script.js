5694f058cdb297f146dfa147c7c90f5846d1209b
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-28 17:22:26 +0200

    Create script.js

---
+++ Aufgabe2.1/Aufgabe5/Aufgabe5e/script.js
@@ -0,0 +1,10 @@
+"use strict";
+function factorial() {
+    let n = 1;
+    while (n > 1) {
+        n = n * (n + 1);
+        console.log(n);
+    }
+    return n;
+}
+//# sourceMappingURL=script.js.map
