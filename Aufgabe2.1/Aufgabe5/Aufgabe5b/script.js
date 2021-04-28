55950ae9a77629e4807f1461054a7dc41fccee08
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-28 17:21:41 +0200

    Create script.js

---
+++ Aufgabe2.1/Aufgabe5/Aufgabe5b/script.js
@@ -0,0 +1,12 @@
+"use strict";
+function max() {
+    let y = 5;
+    let u = 3;
+    if (y < u) {
+        console.log(u);
+    }
+    else {
+        console.log(y);
+    }
+}
+//# sourceMappingURL=script.js.map
