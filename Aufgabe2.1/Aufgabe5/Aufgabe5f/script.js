5557c70d46f0f64fcff7978fc79bfcb9033af427
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-28 23:36:57 +0200

    Update script.js

--- Aufgabe2.1/Aufgabe5/Aufgabe5f/script.js
+++ Aufgabe2.1/Aufgabe5/Aufgabe5f/script.js
@@ -1,2 +1,21 @@
 "use strict";
+function leapyears() {
+    for (let i = 1900; i < 2021; i++) {
+        let schaltjahr = false;
+        if (i % 4 == 0) {
+            if (i % 100 == 0) {
+                if (i % 400 == 0) {
+                    schaltjahr = true;
+                }
+            }
+            else {
+                schaltjahr = true;
+            }
+        }
+        if (schaltjahr) {
+            console.log(i);
+        }
+    }
+}
+leapyears();
 //# sourceMappingURL=script.js.map
