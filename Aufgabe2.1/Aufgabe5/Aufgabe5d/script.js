cf60bf7afb3f4b5587908183ab5a55862fcca147
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-28 23:36:40 +0200

    Update script.js

--- Aufgabe2.1/Aufgabe5/Aufgabe5d/script.js
+++ Aufgabe2.1/Aufgabe5/Aufgabe5d/script.js
@@ -1,7 +1,8 @@
 "use strict";
 function getRandomInt(_a, _b) {
     for (let i = 0; i < 100; i++) {
-        return Math.random();
+        let zufall = Math.random() * 100;
+        console.log(zufall);
     }
 }
 //# sourceMappingURL=script.js.map
