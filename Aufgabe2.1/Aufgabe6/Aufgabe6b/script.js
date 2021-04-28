86a65977686d606d91b3ad65cecaf1904ec661bf
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-28 23:37:16 +0200

    Update script.js

--- Aufgabe2.1/Aufgabe6/Aufgabe6b/script.js
+++ Aufgabe2.1/Aufgabe6/Aufgabe6b/script.js
@@ -1,10 +1,9 @@
 "use strict";
-//B
 for (let i = 1; i <= 100; i++) {
-    if (i % 3) {
+    if (i % 3 == 0) {
         console.log("Fizz");
     }
-    if (i % 5 & i % 3) {
+    else if (i % 5 == 0) {
         console.log("Buzz");
     }
     else {
