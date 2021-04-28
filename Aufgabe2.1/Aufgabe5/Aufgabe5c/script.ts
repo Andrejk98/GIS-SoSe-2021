9ee28a976d868f285d7bb1a8a3c179e892ce660c
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-28 17:40:55 +0200

    Update script.ts

--- Aufgabe2.1/Aufgabe5/Aufgabe5c/script.ts
+++ Aufgabe2.1/Aufgabe5/Aufgabe5c/script.ts
@@ -1,8 +1,10 @@
 function count(): void {
     let x: number = 0;
+    let a: number = 0;
     while ( x < 100 ) {
         x++;
-        console.log(x);
+        a = x + a;
+        console.log(a);
     }
 }
 count();
