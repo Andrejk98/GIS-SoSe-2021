81793ed047f8de434770a7d004d8ff0d65c88c3d
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-04-30 09:37:27 +0200

    Update script.js

--- Aufgabe2.2/Aufgabe1/Aufgabe1a/script.js
+++ Aufgabe2.2/Aufgabe1/Aufgabe1a/script.js
@@ -1,2 +1,11 @@
 "use strict";
+/*function min(): number {
+    let a: number = 10;
+    let b: number = 20;
+    let c: number = 500;
+
+
+    return Number;
+}
+*/ 
 //# sourceMappingURL=script.js.map
