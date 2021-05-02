effacd45e804e4286d9f8ed66a49bba6827844d1
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-05-02 17:23:08 +0200

    a

---
+++ Aufgabe2.2/Aufgabe3/Aufgabe3a/script.ts
@@ -0,0 +1,15 @@
+let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("myCanvas");
+let context: CanvasRenderingContext2D = canvas.getContext("2d");
+
+context.lineWidth = 10;
+
+context.strokeRect(75, 140, 150, 110);
+
+context.fillRect(130, 190, 40, 60);
+
+context.beginPath();
+context.moveTo(50, 140);
+context.lineTo(150, 60);
+context.lineTo(250, 140);
+context.closePath();
+context.stroke();
