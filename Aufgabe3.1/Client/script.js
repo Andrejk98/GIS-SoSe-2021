0eb6e26c5f5f8794e60ab898474a044b4860696c
Andrejk98 <80950487+Andrejk98@users.noreply.github.com>
2021-06-04 14:02:25 +0200

    a

---
+++ Aufgabe3.1/Client/script.js
@@ -0,0 +1,25 @@
+"use strict";
+var Aufgabe3_1;
+(function (Aufgabe3_1) {
+    //Synchrone Funktion SendData, welche die URL erweitert
+    function sendData() {
+        //Daten asus Formular auslesen
+        let formData = new FormData(document.forms[0]);
+        let url = "https://gis-sose-2021-Andrejk98.herokuapp.com/";
+        // tslint:disable-next-line: no-any
+        let query = new URLSearchParams(formData);
+        //query an die Url anhängen
+        url = url + "?" + query.toString();
+        //asynchrone Funktion communicate aufrufen, welche die Anfrage per fetch an den server schickt
+        communicate(url);
+    }
+    document.querySelector("#sendData").addEventListener("click", sendData);
+    async function communicate(_url) {
+        //Anfrage an den Server
+        let response = await fetch(_url);
+        //Antwort vom server
+        let responseString = await response.text();
+        console.log(responseString);
+    }
+})(Aufgabe3_1 || (Aufgabe3_1 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe3.1/Client/test.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Hallo");
+function test() {
+    let x = 10;
+    let a = ["hallo", "dfkf"];
+    console.log(a[1], x);
+}
+test();
+//# sourceMappingURL=test.js.map
